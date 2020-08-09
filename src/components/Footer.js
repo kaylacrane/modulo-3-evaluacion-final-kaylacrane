dcb60907fc747bed3e1263f60e05bd4d39a28b25
Kayla Jean Crane <kayla.crane@gmail.com>
2020-08-09 11:28:44 +0200

    add wrapper and adjust footer CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,23 @@ class Footer extends Component {
     const githubLink = 'https://github.com/kaylacrane';
     return (
       <footer className="footer">
-        <div className="copy-block">
-          <small>
-            <span role="img" aria-label="nerd emoji">
-              🤓
-            </span>
-            <a href={githubLink} title="Kayla's GitHub page">
-              kayla
-            </a>{' '}
-            made this
-          </small>
-        </div>
-        <div className="logo-block">
-          <a href="https://adalab.es/" title="Visit Adalab.es">
-            <img src={adalabLogo} alt="Adalab logo" />
-          </a>
+        <div className="wrapper">
+          <div className="copy-block">
+            <small>
+              <span role="img" aria-label="nerd emoji">
+                🤓
+              </span>
+              <a href={githubLink} title="Kayla's GitHub page">
+                kayla
+              </a>{' '}
+              made this
+            </small>
+          </div>
+          <div className="logo-block">
+            <a href="https://adalab.es/" title="Visit Adalab.es">
+              <img src={adalabLogo} alt="Adalab logo" />
+            </a>
+          </div>
         </div>
       </footer>
     );
